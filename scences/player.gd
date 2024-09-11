c4db09756dd997f551bc27b9dfefdf168c26d41b
Jhonatan <jhonatanyamilcabezas@gmail.com>
2024-09-11 16:52:27 -0400

    Rope one node

--- scences/player.gd
+++ scences/player.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
 @export var isPlayer1 = true
+#@export var other_character: CharacterBody2D
 
-var SPEED = 150			
+var SPEED = 150
 var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity")
 var JUMP_SPEED = -600
+var MAX_DISTANCE = 50 
+var PULL_FORCE = 500
 
 var input_vector
 
@@ -12,12 +15,14 @@ func _ready() -> void:
 	if isPlayer1:
 		input_vector = [
 			"Right player 1",
-			"Left player 1"
+			"Left player 1",
+			"Up player 1"
 		]
 	else:
 		input_vector = [
 			"Right player 2",
-			"Left player 2"
+			"Left player 2",
+			"Up player 2"
 		]
 
 func _physics_process(delta: float) -> void:
@@ -25,7 +30,16 @@ func _physics_process(delta: float) -> void:
 	velocity.x = right * SPEED
 	velocity.y += GRAVITY * delta
 	
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_SPEED 
+	if Input.is_action_just_pressed(input_vector[2]) and is_on_floor():
+		velocity.y = JUMP_SPEED  
 		
 	move_and_slide()
+	#
+	#if other_character:
+		#var distance = position.distance_to(other_character.position)
+		#if distance > MAX_DISTANCE:
+			#var pull_direction = (other_character.position - position).normalized()
+			#velocity += pull_direction * PULL_FORCE * delta
+			#other_character.velocity -= pull_direction * PULL_FORCE * delta
+	#else:
+		#print("other_character no está asignado")
---
+++ scripts/main.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@onready var player_1: CharacterBody2D = $Player1
+@onready var player_2: CharacterBody2D = $Player2
+@onready var rope: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if rope:
+		# Instancia la cuerda desde la escena empaquetada
+		var rope_instance = rope.instantiate()
+		add_child(rope_instance)
+		
+		# Obtiene los nodos PinJoint2D en la cuerda
+		var connection1 = rope_instance.get_node("PinJoint2DPlayer1")
+		var connection2 = rope_instance.get_node("PinJoint2DPlayer2")
+
+		if connection1 and connection2:
+			# Conecta los extremos de la cuerda a los personajes
+			connection1.node_a = player_1
+			connection1.node_b = rope_instance.get_node("RigidBodyP1")
+			connection2.node_a = player_2
+			connection2.node_b = rope_instance.get_node("RigidBodyP2")
+	else:
+		print("Rope scene is not assigned.")
+		
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
